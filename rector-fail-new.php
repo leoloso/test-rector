714a187e34aac685251505a4256123389835aac7
Leonardo Losoviz <leo@getpop.org>
2021-10-20 19:37:39 +0800

    Added configs

---
+++ rector-fail-new.php
@@ -0,0 +1,8 @@
+<?php
+
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(DowngradeLevelSetList::DOWN_TO_PHP_71);
+};
